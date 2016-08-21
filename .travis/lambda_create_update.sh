55c5f4d831c10dc7aa8660cb9cbb474ea6e6030c
vandiedakaf <vandiedakaf@gmail.com>
2016-08-21 16:09:06 +0200

    cli

--- .travis/lambda_create_update.sh
+++ .travis/lambda_create_update.sh
@@ -6,14 +6,27 @@ S3_KEY="lambda/twilio2slack-1.0-SNAPSHOT.zip"
 
 echo "Creating $FUNC"
 aws lambda create-function --function-name $FUNC --runtime java8 --role arn:aws:iam::522161570381:role/dev-lambda-LambdaExecutionRole-17S53XUZ6GVT --handler ForwardSms::processSms --code S3Bucket=$S3_BUCKET,S3Key=$S3_KEY --output json
-$ echo $?
+if [ $? -eq 0 ]
+then
+  echo "Successfully created function"
+else
+  echo "Updating $FUNC"
+  echo "aws lambda update-function-code --function-name $FUNC --s3-bucket $S3_BUCKET --s3-key $S3_KEY --output json"
+  aws lambda update-function-code --function-name $FUNC --s3-bucket $S3_BUCKET --s3-key $S3_KEY --output json
 
-echo "Updating $FUNC"
-echo "aws lambda update-function-code --function-name $FUNC --s3-bucket $S3_BUCKET --s3-key $S3_KEY --output json"
-aws lambda update-function-code --function-name $FUNC --s3-bucket $S3_BUCKET --s3-key $S3_KEY --output json
-$ echo $?
+    if [ $? -eq 0 ]
+    then
+        echo "Successfully updated function"
+    else
+        echo "Failed to update lambda"
+        exit 1
+    fi
+fi
 
 echo "Invoking $FUNC"
-echo "aws lambda invoke --function-name $FUNC --payload 'Hello World' --log-type Tail --output json"
-aws lambda invoke --function-name $FUNC --payload 'Hello World' --log-type Tail --output json
-$ echo $?
+echo "aws lambda invoke --function-name $FUNC --payload 'HelloWorld' --log-type Tail --output json"
+aws lambda invoke --function-name $FUNC --payload 'HelloWorld' --log-type Tail --output json
+echo $?
+
+echo "Successfully created or updated lambda"
+exit 0
