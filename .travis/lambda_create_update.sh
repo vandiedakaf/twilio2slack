c2730414efc132f21dffc5fd60fb742560b467f7
vandiedakaf <vandiedakaf@gmail.com>
2016-08-21 15:48:24 +0200

    cli

--- .travis/lambda_create_update.sh
+++ .travis/lambda_create_update.sh
@@ -6,6 +6,6 @@ S3_KEY="lambda/twilio2slack-1.0-SNAPSHOT.zip"
 
 aws lambda create-function --function-name $FUNC --runtime java8 --role arn:aws:iam::522161570381:role/dev-lambda-LambdaExecutionRole-17S53XUZ6GVT --handler ForwardSms::processSms --code S3Bucket=$S3_BUCKET,S3Key=$S3_KEY --output json
 
-aws lambda update-function-code --function-name $FUNC --s3-bucket $S3_BUCKET --s3-key
+aws lambda update-function-code --function-name $FUNC --s3-bucket $S3_BUCKET --s3-key $S3_KEY --output json
 
-aws lambda invoke --function-name $FUNC --payload "Hello World"
+aws lambda invoke --function-name $FUNC --payload "Hello World" --log-type Tail --output json
