bfa13f036d01b8d19568cf2adda368a7575b9f4f
vandiedakaf <vandiedakaf@gmail.com>
2016-08-21 15:33:05 +0200

    cli

---
+++ .travis/lambda_create_update.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+aws lambda create-function --function-name blambda --runtime java8 --role arn:aws:iam::522161570381:role/dev-lambda-LambdaExecutionRole-17S53XUZ6GVT --handler ForwardSms::processSms --code S3Bucket=vdda-sandbox,S3Key=lambda/twilio2slack-1.0-SNAPSHOT.zip --output text
