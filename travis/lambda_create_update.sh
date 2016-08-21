29c4c69815797f3f3c994b5104c576a6958d4f27
vandiedakaf <vandiedakaf@gmail.com>
2016-08-21 20:40:15 +0200

    script update

--- travis/lambda_create_update.sh
+++ travis/lambda_create_update.sh
@@ -5,7 +5,7 @@ POLICY="lambda-basic"
 ROLE="dev-lambda-role"
 S3_KEY="lambda/twilio2slack-1.0-SNAPSHOT.zip"
 
-
+# assume that the policy also exists if the role exists
 aws iam get-role --role-name ${ROLE}
 if [ $? -ne 0 ]
 then
@@ -15,6 +15,13 @@ then
     POLICY_ARN=$(aws iam create-policy --policy-document file://travis/policy_lambda_basic.json --output json --policy-name ${POLICY} | jq -r .Policy.Arn)
     echo "Policy ARN: ${POLICY_ARN}"
     aws iam attach-role-policy --role-name ${ROLE} --policy-arn ${POLICY_ARN}
+    # sleep so that roles and policies can propagate through system otherwise the lambda won't be able to use it
+    sleep 10s
+else
+    ROLE_ARN=$(aws iam get-role --role-name ${ROLE} | jq -r .Role.Arn)
+    echo "Role ARN: ${ROLE_ARN}"
+    POLICY_ARN=$(aws iam get-policy --policy-name ${POLICY} | jq -r .Policy.Arn)
+    echo "Policy ARN: ${POLICY_ARN}"
 fi
 
 aws lambda get-function --function-name ${FUNC}
