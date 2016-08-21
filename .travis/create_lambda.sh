326902bf8ef8b5a4edf6f29133c9bec12b44d8b1
vandiedakaf <vandiedakaf@gmail.com>
2016-08-21 13:49:52 +0200

    rekey

--- .travis/create_lambda.sh
+++ .travis/create_lambda.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws lambda create-function --function-name blambda --runtime java8 --role dev-lambda-LambdaExecutionRole-17S53XUZ6GVT --handler FrowardSms::processSms --code S3Bucket=vdda-sandbox,S3Key=twillio2slack-1.0-SNAPSHOT.zip
+aws lambda create-function --function-name blambda --runtime java8 --role arn:aws:iam::522161570381:role/dev-lambda-LambdaExecutionRole-17S53XUZ6GVT --handler FrowardSms::processSms --code S3Bucket=vdda-sandbox,S3Key=twillio2slack-1.0-SNAPSHOT.zip
