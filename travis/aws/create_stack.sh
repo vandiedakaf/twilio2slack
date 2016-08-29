c8dfb0ccf9d3fc93d6f2d3a3d5ec534becb640bc
vandiedakaf <vandiedakaf@gmail.com>
2016-08-29 23:45:24 +0200

    smashing

---
+++ travis/aws/create_stack.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+STACK_NAME=twilio2slack
+
+echo
+echo Searching for stack
+STACK_SEARCH=$(aws cloudformation list-stacks --stack-status-filter CREATE_IN_PROGRESS CREATE_COMPLETE DELETE_IN_PROGRESS ROLLBACK_COMPLETE| jq -r ".StackSummaries[] | select(.StackName==\"${STACK_NAME}\")")
+
+#true if empty
+if [ -z "$STACK_SEARCH" ]
+then
+    echo Creating stack...
+    CREATE_STACK=$(aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://api_gateway.template)
+    echo Stack templated uploaded.
+else
+    echo Stack already exists.
+fi
--- travis/aws/delete_stack.sh
+++ travis/aws/delete_stack.sh
@@ -2,13 +2,16 @@
 
 STACK_NAME=twilio2slack
 
+echo
+echo Searching for stack
 STACK_SEARCH=$(aws cloudformation list-stacks --stack-status-filter CREATE_IN_PROGRESS CREATE_COMPLETE DELETE_IN_PROGRESS ROLLBACK_COMPLETE| jq -r ".StackSummaries[] | select(.StackName==\"${STACK_NAME}\")")
 
 #true if empty
 if [ -z "$STACK_SEARCH" ]
 then
-    echo Creating stack...
-    CREATE_STACK=$(aws cloudformation create-stack --stack-name ${STACK_NAME} --template-body file://api_gateway.template)
+    echo Stack does not exist.
 else
-    echo Stack already exists.
+    echo Deleting stack...
+    CREATE_STACK=$(aws cloudformation delete-stack --stack-name ${STACK_NAME})
+    echo Delete request submitted.
 fi
