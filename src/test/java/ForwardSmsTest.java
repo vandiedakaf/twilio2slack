58551aeb95538a0d1b8664281cc053f5d459e7bd
vandiedakaf <vandiedakaf@gmail.com>
2016-09-02 00:08:28 +0200

    Empty tests added

---
+++ src/test/java/ForwardSmsTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by francois on 2016-09-02.
+ */
+public class ForwardSmsTest {
+    @org.junit.Test
+    public void handleRequest() throws Exception {
+    }
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}
